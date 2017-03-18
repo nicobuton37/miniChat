6619a7cb0c97b082c682c6bd1fee18643a047824
nicobuton37 <nicobuton370@gmail.com>
2017-03-18 15:29:55 +0100

    coucou

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include "views/header.php"; ?>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Entrez votre Pseudo</div>
+            <div class="panel-body">
+                <div class="form-group">
+                    <form action="" method="post">
+                        <input type="text" name="pseudo" placeholder="Pseudo" class="form-control">
+                        <button type="submit" name="pseudo" class="btn btn-success">Envoyer</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include "views/footer.php"; ?>
