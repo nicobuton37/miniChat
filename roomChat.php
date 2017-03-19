c0a73bb54757fb0e0e2b8e103c02d663d34febf8
nicobuton37 <nicobuton370@gmail.com>
2017-03-19 15:40:51 +0100

    insert message in db

--- roomChat.php
+++ roomChat.php
@@ -5,6 +5,9 @@
 <?php
 $queryName = $db->query('SELECT name FROM users');
 $name = $queryName->fetch();
+$queryId = $db->query('SELECT id FROM users');
+$idUser = $queryId->fetch();
+
 ?>
 
 <div class="form-group">
@@ -12,10 +15,19 @@ $name = $queryName->fetch();
         <input type="text" name="pseudo" value='<?= $name['name'];?>' disabled="true" class="form-control">
         <textarea name="message" rows="8" cols="80" placeholder="Votre message" class="form-control"></textarea>
         <button type="submit" name="sendMess" class="btn btn-info">Poster</button>
+        <?php if(isset($_POST['message'])){
+            $insert = $db->prepare("INSERT INTO messages(id_user, message, date_pub)VALUES(:id_user, :message, :date_pub)");
+            $insert->execute(array(
+                'id_user' => $idUser['id'],
+                'message' => $_POST['message'],
+                'date_pub' => date('Y-m-d H:i:s')
+            ));
+        }?>
     </form>
 </div>
 <div>
     <?= buildRoom($name['name']); ?>
+
 </div>
 
 
